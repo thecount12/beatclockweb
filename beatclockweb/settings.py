d4e9d51484dce01a904eaa34608f7709dc8043aa
William Gunnells <gunnells@gmail.com>
2020-07-14 17:54:57 -0700

    link and page changes

--- beatclockweb/settings.py
+++ beatclockweb/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
 
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'basicapp',
     'signup',
     'blog',
